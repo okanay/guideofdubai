9fea8f6be04de66758ed3b1ac1281e3e156f3379
Okan Ay <okanay@Okan-MacBook-Pro-2.local>
2025-01-16 19:38:23 +0300

    test 6

--- src/assets/scripts/visa-ship.ts
+++ src/assets/scripts/visa-ship.ts
@@ -1,1 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {})
+import { ModalController } from './packages/modal.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (typeof window !== 'undefined') {
+    window.callIcons()
+  }
+
+  new ModalController(
+    [
+      {
+        id: 'visa-ship-modal',
+        contentElement: '#visa-ship-modal',
+        containers: ['#visa-ship-modal-container'],
+        openElements: ['#visa-ship-modal-open-button'],
+        closeElements: ['#visa-ship-modal-closed-button'],
+        toggleElements: [],
+      },
+    ],
+    {
+      outsideClickClose: true,
+      escapeClose: true,
+      preserveModalHistory: true,
+      attributes: {
+        stateAttribute: 'data-state',
+        values: {
+          open: 'open',
+          preserved: 'open',
+          hidden: 'closed',
+        },
+      },
+      scrollLock: {
+        enabled: true,
+        styles: {
+          hidden: {
+            overflow: 'hidden',
+            position: 'fixed',
+            width: '100%',
+          },
+          visible: {
+            overflow: 'auto',
+            position: 'static',
+            width: 'auto',
+          },
+        },
+      },
+    },
+  )
+})
